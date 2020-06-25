fee64df84a10870010564a9820cd29b30afc249f
marco <marcosigurta.ovb@gmail.com>
2020-06-25 15:20:17 +0200

    Style Changes

--- bonus/js/main.js
+++ bonus/js/main.js
@@ -1,5 +1,10 @@
-for (var i = 1; i <= 100; i++) {
+var btn = document.getElementById('btn');
+btn.addEventListener("click", function () {
+    document.getElementById("list").classList = "block";
+})
 
+
+for (var i = 1; i <= 100; i++) {
     if ((i % 3) && !(i % 5)) {
         document.getElementById('list').innerHTML += "<li> Fizz </li>";
     } else if (!(i % 3) && (i % 5)) {
