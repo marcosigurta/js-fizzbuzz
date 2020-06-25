d38fc494b927702dffc36f0563c2b2857e61d7f5
marco <marcosigurta.ovb@gmail.com>
2020-06-25 14:57:44 +0200

    FizzBuzz Js game

---
+++ bonus/js/main.js
@@ -0,0 +1,12 @@
+for (var i = 1; i <= 100; i++) {
+
+    if ((i % 3) && !(i % 5)) {
+        document.getElementById('list').innerHTML += "<li> Fizz </li>";
+    } else if (!(i % 3) && (i % 5)) {
+        document.getElementById('list').innerHTML += "<li> Buzz </li>";
+    } else if (!(i % 3) && !(i % 5)) {
+        document.getElementById('list').innerHTML += "<li> FizzBuzz </li>";
+    } else {
+        document.getElementById('list').innerHTML += "<li>" + i + "</li>";
+    }
+}
